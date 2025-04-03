51f284aad3e7b9010a2c301b03823245ed4346ad
Nowell Phelps <nowell.phelps@gmail.com>
2025-04-02 23:04:48 -0400

    main figures (besides violent crime) complete

--- data_analysis_task.R
+++ data_analysis_task.R
@@ -17,6 +17,7 @@ library(tidyverse)
 library(viridis)
 library(grid)
 library(gridExtra)
+library(boot)
 
 outdir_folder <- "~/Desktop/Hopkins/Year 1/Methods/Term 4/Baltimore-crime/Figure/"
 dir.create(outdir_folder, showWarnings = F)
@@ -42,6 +43,11 @@ data_subset$hour <- hour(data_subset$datetime)
 data_subset$day <- day(data_subset$datetime)
 data_subset$days_in_month <- lubridate::days_in_month(data_subset$datetime)
 data_subset$weekday <- wday(data_subset$datetime, label = T)
+
+
+# Classify crime as violent or non-violent
+violent_crime_types <- c("AGG. ASSAULT", "ASSAULT BY THREAT", "COMMON ASSAULT", "HOMICIDE", "RAPE", "SHOOTING")
+data_subset$crime_violent <- ifelse(data_subset$Description %in% violent_crime_types, "violent", "not violent")
 
 ################################################################################
 # Data cleaning and potential duplicates removal
@@ -98,13 +104,6 @@ rate_hour <- data.frame(datetime = seq(min(data_subset$year_month_day_hour), max
 
 # Convert NA to 0 (no crimes recorded in hour)
 rate_hour$count[which(is.na(rate_hour$count))] <- 0
-# 
-# rate_hour <- rate_hour %>%
-#   mutate(rate = count) %>%
-#   filter(!(year == 2015 & month == "Apr" & day == 27))
-
-# resent "days in month" in april 2015 to 29, to account for removal of April 27 2015
-#rate_hour$days_in_month[which(rate_hour$month == "Apr" & rate_hour$year == 2015)] <- 29
 
 # Get rate by day - start with skeleton dataset, merge to count dataset, and set NA to 0 then calculate rate
 rate_day <- data.frame(datetime = seq(min(data_subset$year_month_day_hour), max(data_subset$year_month_day_hour), by = 'day')) %>%
@@ -120,21 +119,14 @@ rate_day <- data.frame(datetime = seq(min(data_subset$year_month_day_hour), max(
 # Convert NA to 0 (no crimes recorded in hour)
 rate_day$count[which(is.na(rate_hour$day))] <- 0
 
-# rate_day <- rate_day %>%
-#   mutate(rate = count) %>%
-#   filter(!(year == 2015 & month == "Apr" & day == 27))
-
-# resent "days in month" in april 2015 to 29, to account for removal of April 27 2015
-# rate_day$days_in_month[which(rate_day$month == "Apr" & rate_day$year == 2015)] <- 29
-         
-
 ################################################################################
 # EXPLORATORY DATA ANALYSIS                                                    #
 ################################################################################
 
 ################################################################################
+# Figure 1 - Crime rate by year and month
 fig1a <- ggplot(data_subset, aes(x = datetime)) +
-  geom_histogram(stat = "bin", binwidth = 60*60*24, fill = "grey50", alpha = 0.8) +
+  geom_histogram(stat = "bin", binwidth = 60*60*24, alpha = 0.8) +
   theme_classic() +
   scale_x_datetime(breaks = as.POSIXct(paste0(seq(2012, 2017), "-01-01 00:00:00")), 
                    labels = seq(2012, 2017),
@@ -147,8 +139,6 @@ fig1a <- ggplot(data_subset, aes(x = datetime)) +
         axis.title =element_text(size = 14), 
         title = element_text(size = 16))
 
-################################################################################
-# Figure 2 - Average crime rate by year and month
 data_plot <- rate_day %>%
   group_by(month, year, days_in_month) %>%
   summarise(tot_count = sum(count), 
@@ -190,7 +180,6 @@ fig1c <- ggplot(data_plot, aes(x = month, y = rate, colour = year, group = year)
         title = element_text(size = 16)) 
 
 
-
 # PLOT FIGURE 1
 figNum <- 1
 appendix <- F
@@ -217,8 +206,129 @@ grid.arrange(
 
 dev.off()
 
+
+
 ################################################################################
-# Figure 3 - Crime rate by month and day of month
+# Figure 2  - Crime rate by hour and month over entire period
+data_mean_rate <- rate_hour %>%
+  group_by(hour, month) %>%
+  summarise(tot_days = sum(days_in_month)/24, 
+            tot_count = sum(count)) %>%
+  mutate(mean_rate = tot_count/tot_days)
+
+data_plot <- left_join(data_mean_rate, rate_hour, by = c("month", "hour"))
+
+
+p2 <- ggplot(data_plot, aes(x = factor(hour), y = count, fill = mean_rate)) +
+  facet_wrap(~month) + 
+  geom_violin() +
+  scale_fill_gradientn(colors = rev(heat.colors(100)))+
+  scale_x_discrete(breaks = c(0, 6, 12, 18),
+                   labels = c("12am", "6pm", "12pm", "6pm")) +
+  theme_minimal() +
+  labs(x = "Hour of day", 
+       y = "Crime rate (crimes per hour)", 
+       fill = "Mean hourly\ncrime rate") +
+  theme(axis.text = element_text(size = 12), 
+        axis.text.x = element_text(angle = 60, hjust = 1),
+        axis.title =element_text(size = 14))
+
+figNum <- 2
+appendix <- F
+figsuffix <- ""
+blank <- grid.rect(gp=gpar(col=NA, fill = NA))
+cairo_pdf(paste0(outdir_folder, ifelse(appendix, "Appendix Figure ", "Figure "), figNum, figsuffix,".pdf"), height = 10, width = 12, onefile=T)
+grid.arrange(
+  arrangeGrob(blank,
+              arrangeGrob(
+                blank, 
+                p2,
+                blank, 
+                nrow =1, 
+                widths = c(1, 10, 1)),
+              blank,
+              ncol = 1,
+              heights = c(1, 10, 1))
+)
+dev.off()
+
+
+
+################################################################################
+# Figure 3 - Hourly crime rate stratified by month and day of week
+data_mean_rate <- rate_hour %>%
+  group_by(hour, month) %>%
+  summarise(tot_days = sum(days_in_month)/24, 
+            tot_count = sum(count)) %>%
+  mutate(mean_rate = tot_count/tot_days)
+
+data_plot <- left_join(data_mean_rate, rate_hour, by = c("month", "hour")) %>% 
+  select(hour, month, mean_rate) %>% 
+  unique()
+
+p3a <- ggplot(data_plot, aes(x = factor(hour), y = mean_rate, colour = month, group = month)) +
+  geom_line() +
+  scale_colour_discrete(name = "Month") +
+  scale_x_discrete(breaks = c(0, 3, 6, 9, 12, 15, 18, 21),
+                   labels = c("12am", "3am", "6am","9am", "12pm", "3pm", "6pm","9pm")) +
+  theme_classic() +
+  labs(x = "Hour of day", 
+       y = "Crime rate (crimes per hour)") 
+
+data_numb <- rate_hour %>%
+  mutate(hour_weekday = paste(hour, weekday)) %>%
+  select(hour_weekday) %>%
+  group_by(hour_weekday) %>%
+  summarise(numb_hours_in_hour_weekday = n()) 
+
+data_plot <- rate_hour %>%
+  mutate(hour_weekday = paste(hour, weekday)) %>%
+  group_by(hour, weekday, hour_weekday) %>%
+  summarise(tot_count = sum(count)) %>%
+  left_join(data_numb, by = "hour_weekday") %>%
+  mutate(rate = tot_count/numb_hours_in_hour_weekday) %>%
+  ungroup()
+
+data_plot$weekday <- factor(data_plot$weekday,
+                            levels = c(2,3,4,5,6,7,1),
+                            labels = as.character(wday(c(2:7, 1), label = TRUE)))
+
+p3b <- ggplot(data_plot, aes(x = hour, colour = weekday, y = rate, group = weekday)) +
+  geom_line() +
+  theme_classic() +
+  scale_colour_discrete(name ="Day of week") +
+  scale_x_continuous(breaks = c(0, 3, 6, 9, 12, 15, 18, 21),
+                   labels = c("12am", "3am", "6am","9am", "12pm", "3pm", "6pm","9pm")) +
+  labs(x = "Hour of day",
+       y = "Crime rate (crimes per hour)")
+  
+figNum <- 3
+appendix <- F
+figsuffix <- ""
+blank <- grid.rect(gp=gpar(col=NA, fill = NA))
+cairo_pdf(paste0(outdir_folder, ifelse(appendix, "Appendix Figure ", "Figure "), figNum, figsuffix,".pdf"), height = 6.375, width = 15, onefile=T)
+
+grid.arrange(
+  arrangeGrob(blank,
+              arrangeGrob(
+                arrangeGrob(textGrob("A",hjust=0,just = c("left"),x = unit(0.2, "npc"),gp = gpar(col = "black", fontsize = 20)), blank, ncol = 1, heights = c(1, 20)),
+                p3a, 
+                arrangeGrob(textGrob("B",hjust=0,just = c("left"),x = unit(0.2, "npc"),gp = gpar(col = "black", fontsize = 20)), blank, ncol = 1, heights = c(1, 20)),
+                p3b, 
+                blank, 
+                nrow =1, 
+                widths = c(1, 10, 1, 10, 1)),
+              blank,
+              ncol = 1,
+              heights = c(1, 10, 1))
+)
+
+dev.off()
+
+
+
+################################################################################
+# Figure 4 - Crime rate by month and day of month
 
 # Plots of crime rate by month and day of month
 data_numb <- rate_day %>%
@@ -247,19 +357,14 @@ data_plot <- data_plot %>%
   mutate(rate_centred = rate - mean_rate) %>%
   mutate(rate_type = factor(ifelse(rate_centred <0, "<0", ">=0")))
 
-pa2 <- ggplot(data_plot, aes(x = day, y = rate)) +
-  geom_col() + 
-  facet_wrap(~month) + 
-  theme_minimal() + 
-  theme(axis.text.x = element_text(angle = 60, vjust = 1, hjust=1)) +
-  labs(x = "Day of month",
-       y = "Crime rate (crimes per day)")
-
-p3b <- ggplot(data_plot, aes(x = day, y = rate_centred, fill = rate_type)) +
-  geom_col() +
+p4 <- ggplot(data_plot, aes(x = day, y = rate_centred)) +
+  geom_hline(yintercept = 0) +
+  geom_point(shape = 21, aes(fill = rate_type)) +
+  geom_smooth() +
   facet_wrap(~month) +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 60, vjust = 1, hjust=1)) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 60, vjust = 1, hjust=1), 
+        panel.grid = element_blank()) +
   scale_fill_manual(values = c("<0" = "darkgreen", ">=0" = "red"), 
                     labels = c("<0" = "Below monthly average",">=0" = "Above monthly average"))+
   labs(x = "Day of month",
@@ -267,87 +372,57 @@ p3b <- ggplot(data_plot, aes(x = day, y = rate_centred, fill = rate_type)) +
        #title = "Baltimore crime rate by month and day of week, centred by monthly average",
        fill = "")
 
-
-# Plots of crime rate by month and day of week
-data_numb <- rate_day %>%
-  mutate(month_weekday_year = paste(month, weekday, year)) %>%
-  select(month_weekday_year) %>%
-  unique() %>%
-  mutate(month_weekday =  sub("^([^ ]+ [^ ]+).*", "\\1", month_weekday_year)) %>%
-  group_by(month_weekday) %>%
-  summarise(numb_days_in_month_weekday = n()) 
-
-data_plot <- rate_day  %>%
-  mutate(month_weekday = paste(month, weekday)) %>%
-  group_by(month, weekday, month_weekday) %>%
-  summarise(tot_count = sum(count)) %>%
-  left_join(data_numb, by = "month_weekday") %>%
-  mutate(rate = tot_count/numb_days_in_month_weekday) %>%
-  ungroup()
-
-mean_rate <- data_plot %>%
-  group_by(month) %>%
-  summarise(mean_rate = mean(rate)) %>%
-  ungroup()
-
-data_plot <- data_plot %>%
-  left_join(mean_rate, by = "month") %>%
-  mutate(rate_centred = rate - mean_rate) %>%
-  mutate(rate_type = factor(ifelse(rate_centred <0, "<0", ">=0")))
-
-pa2 <- ggplot(data_plot, aes(x = weekday, y = rate)) +
-  geom_col() + 
-  facet_wrap(~month) + 
-  theme_minimal() + 
-  theme(axis.text.x = element_text(angle = 60, vjust = 1, hjust=1)) +
-  labs(x = "Day of month",
-       y = "Crime rate (crimes per day)")
-
-p3b <- ggplot(data_plot, aes(x = weekday, y = rate_centred, fill = rate_type)) +
-  geom_col() +
-  facet_wrap(~month) +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 60, vjust = 1, hjust=1)) +
-  scale_fill_manual(values = c("<0" = "darkgreen", ">=0" = "red"), 
-                    labels = c("<0" = "Below monthly average",">=0" = "Above monthly average"))+
-  labs(x = "Day of month",
-       y = "Mean-centred average crime rate (crimes per day)",
-       #title = "Baltimore crime rate by month and day of week, centred by monthly average",
-       fill = "")
+figNum <- 4
+appendix <- F
+figsuffix <- ""
+cairo_pdf(paste0(outdir_folder, ifelse(appendix, "Appendix Figure ", "Figure "), figNum, figsuffix,".pdf"), height = 10, width = 13, onefile=T)
+grid.arrange(
+  arrangeGrob(blank,
+              arrangeGrob(
+                blank, 
+                p4,
+                blank, 
+                nrow =1, 
+                widths = c(1, 10, 1)),
+              blank,
+              ncol = 1,
+              heights = c(1, 10, 1))
+)
+dev.off()
 
 
 ################################################################################
-# Figure  - Crime rate by hour and month over entire period
+# Figure 5 - Impact of Freddie Gray event
+data_plot <- rate_day %>% filter(month %in% c("Mar", "Apr", "May")) %>%
+  mutate(year_ind = ifelse(year == 2015, 1, 0))
+data_plot$year_ind = factor(data_plot$year_ind)
 
-data_mean_rate <- rate_hour %>%
-  group_by(hour, month) %>%
-  summarise(tot_days = sum(days_in_month)/24, 
-            tot_count = sum(count)) %>%
-  mutate(mean_rate = tot_count/tot_days)
+fig5 <- ggplot(data_plot, aes(x = day, y = count, colour = year_ind, group = year))+
+  facet_wrap(~month) +
+  scale_colour_manual(values = c("grey50", "blue"),
+                      labels = c("Other year", "2015"),
+                      name = "")+
+  labs(y = "Crime rate (crimes per day)", x = "Day of month")+
+  geom_line(alpha = 0.7) +
+  theme_classic()
 
-data_plot <- left_join(data_mean_rate, rate_hour, by = c("month", "hour"))
+# PLOT FIGURE 1
+figNum <- 5
+appendix <- F
+figsuffix <- ""
+blank <- grid.rect(gp=gpar(col=NA, fill = NA))
 
-p4 <- ggplot(data_plot, aes(x = factor(hour), y = rate, fill = mean_rate)) +
-  facet_wrap(~month) + 
-  geom_violin() +
-  scale_fill_gradientn(colors = rev(heat.colors(100)))+
-  scale_x_discrete(breaks = c(0, 6, 12, 18),
-                   labels = c("12am", "6pm", "12pm", "6pm")) +
-  theme_minimal() +
-  labs(x = "Hour of day", 
-       y = "Hourly crime rate", 
-       fill = "Mean hourly crime rate") +
-  
-  theme(axis.text = element_text(size = 12), 
-        axis.title =element_text(size = 14))
+cairo_pdf(paste0(outdir_folder, ifelse(appendix, "Appendix Figure ", "Figure "), figNum, figsuffix,".pdf"), height = 5, width = 8.5, onefile=T)
 
+grid.arrange(
+  arrangeGrob(
+    blank,
+    fig2,
+    blank,
+    heights = c(1, 10, 1)
+  ))
 
-
-
-
-
-
-
+dev.off()
 
 
 
